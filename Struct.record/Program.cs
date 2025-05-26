1c41c35da5f1d19fc122029f487d3f6ae51ee0a6
vusal016 <mvusal316@gmail.com>
2025-05-26 17:48:44 +0400

    elave edildi

---
+++ Struct.record/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Struct.record
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] stu1 = new Student[]
+            {
+            new Student("Ali", "Veliyev", 12),
+            new Student("Osman", "Veliyev", 17),
+            new Student("Teymur", "Veliyev", 179),
+            new Student("Alibaba", "Veliyev", 1)
+            };
+            Group grup = new Group();
+            foreach (var item in stu1)
+            {
+                grup.AddStudent(item);
+            }
+            Student[] sort = grup.GetAllStudents();
+            foreach (var item in sort)
+            {
+                Console.WriteLine(item);
+
+            }
+           Student[]sorted= grup.Sort();
+            foreach (var item in sorted)
+            {
+                Console.WriteLine(item);
+            }
+
+            Group grup2 = new Group();
+
+        }
+    }
+}
+
+
+
+
+
